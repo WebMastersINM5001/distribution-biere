3007781d559744228d4acefa0070037e349eab9f
kbgratton <kgratton26@gmail.com>
2014-11-06 20:33:02 -0500

    Ajout fichiers aide et retrait fichier xampp

--- Site/includes/connect_DB.php
+++ Site/includes/connect_DB.php
@@ -1,7 +1,10 @@
 <?php
 	// Connect to server and select databse.
 	$conn = oci_connect("ec591549", "CbdKGBkD", "//zeta2.labunix.uqam.ca/baclab");
-	if (!$conn) {
+/*$conn = oci_connect("ec591549", "CbdKGBkD", "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=zeta2.labunix.uqam.ca)(PORT=1521))
+(CONNECT_DATA=(SERVER=DEDICATED)
+(SERVICE_NAME = baclab)))");
+*/	if (!$conn) {
 	   $e = oci_error();
        echo $e['message'], "\n";
        trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
@@ -10,3 +13,5 @@
       print "Connected to Oracle!";
    }
  ?>
+
+ 
