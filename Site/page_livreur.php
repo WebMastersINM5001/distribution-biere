e68966edd2a0afd466111ab92f7fcf908f6177b9
Bassem450 <elhakimbp@gmail.com>
2014-11-10 20:13:26 -0500

    debut de la page livreur

--- Site/page_livreur.php
+++ Site/page_livreur.php
@@ -10,7 +10,36 @@ if( !isset($_SESSION["myusername"]) ){
 <?php include("includes/header.php"); ?>
 
 <body>
-Bienvenue cher livreur
+	<script type="text/javascript">
+		function swapClientContent(param){
+			var url = "ajax/phpClientScript.php";
+			$.post(url, {contentVar: param}, function(data){
+				$("#myDiv").html(data).show();
+			});
+		}
+	</script>
+	<header>
+		<div class="container">
+			<p>info du livreur</p>
+		</div>
+	</header>
+
+	<div class="container main">
+		<div class="row">
+			<div class="col-md-3 menu">
+				<a href="#" class="btn btn-default" onclick="return false" onmousedown="javascript:swapClientContent('con1')">Afficher livraison</a>
+			</div>
+			<div id="myDiv" class="col-md-9">
+
+			</div>
+		</div>
+	</div>
+
+	<footer>
+		<div class="container">
+			<p>Propulsé par Web master inc.</p>
+		</div>
+	</footer>
 </body>
 
 <?php include("includes/footer.php"); ?>
