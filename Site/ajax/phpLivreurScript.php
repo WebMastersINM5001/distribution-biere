fc72ac4d86b4706c6b004c1c65f76d19cbd7e799
Bassem450 <elhakimbp@gmail.com>
2014-11-13 22:17:42 -0500

    tentative 2

--- Site/ajax/phpLivreurScript.php
+++ Site/ajax/phpLivreurScript.php
@@ -1,19 +1,32 @@
-<?php 
+<?php
+	include("../includes/databaseInfo.php"); 
+	//session_start();
 	include("../includes/connect_DB.php");
-	date_default_timezone_set('UTC');
+	//date_default_timezone_set('UTC');
 	$contentVar = $_POST['contentVar'];
+
+
 
 	if($contentVar == "con1"){
 
-		//pas a la bonne place, test, marche pas
-		$query = 'select * from USAGER';
+		$myusername=$_POST['userName']; 
+		$mypassword=$_POST['password'];
+
+		$query = 'select * from USAGER where lower(username)= lower(:usr) and lower(password)=lower(:pwd)';
 		$stid = oci_parse($conn, $query);
-		//oci_bind_by_name($stid, ":usr", $myusername);
-		//oci_bind_by_name($stid, ":pwd", $mypassword);
+		oci_bind_by_name($stid, ":usr", $myusername);
+		oci_bind_by_name($stid, ":pwd", $mypassword);		
 		oci_execute($stid);
 		$count = oci_fetch_all($stid, $row);
-    	echo $query;
-    	echo $row['NOUSAGER'][0];
+
+		if($count==1) {
+			echo 'caca';
+		}
+
+    	//echo $row['NOUSAGER'];
+ 
+		//oci_free_statement($stid);
+		//oci_close($conn);
 
 		echo '
 			<h1>'. date("Y/m/d") .'</h1>
